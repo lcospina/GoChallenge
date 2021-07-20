8d10e40e0c07c10b5fe5154c4614257757405421
lcospina <lcospina9@gmail.com>
2021-07-20 13:36:37 -0500

    Comit

--- src/main/frameworks/serverhttp/userfunctions/GetAllUsers.go
+++ src/main/frameworks/serverhttp/userfunctions/GetAllUsers.go
@@ -2,13 +2,13 @@ package userfunctions
 
 import (
 	"GoChallenge/src/main/frameworks/repositoriesimplements"
-	"GoChallenge/src/usecases/usecases"
+	"GoChallenge/src/usecases/userusecases"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func GetAllUsers(c *gin.Context) {
 	var repositoryImp = new(repositoriesimplements.UserRepositoryMysql)
-	users := usecases.GetAllUsers(repositoryImp)
+	users := userusecases.GetAllUsers(repositoryImp)
 	c.JSON(http.StatusOK, &users)
 }
