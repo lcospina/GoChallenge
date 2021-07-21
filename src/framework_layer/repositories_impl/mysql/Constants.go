b38a51a75f9d294474b15a0fe034c82423b15620
lcospina <lcospina9@gmail.com>
2021-07-21 12:09:25 -0500

    Final Commit

--- src/framework_layer/repositories_impl/mysql/Constants.go
+++ src/framework_layer/repositories_impl/mysql/Constants.go
@@ -1,3 +1,6 @@
 package mysql
 
+//ESTO SE PUEDE TRABAJAR CON VARIABLES ENV.
 const DSNMYSQL = "YqVDIko5Db:ASYbSYnDlP@tcp(remotemysql.com:3306)/YqVDIko5Db?parseTime=true"
+
+//const DSNMYSQL = "jikkosoft:aYyigbNAs12YozoB@tcp(localhost:3306)/DBJikkosoft?parseTime=true"
